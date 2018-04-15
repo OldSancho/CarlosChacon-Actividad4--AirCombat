bd615f0b0ff196f2c678525c9b247cea97571d27
Carlos Chacon Lozano <theoldsancho@gmail.com>
2018-04-14 23:28:09 -0500

    Add new repository

---
+++ CarlosChacon-Actividad4-AirCombat/Assets/Scripts/Launch.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Launch : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {	
+
+		//mover el avion
+		if (Input.GetKey (KeyCode.UpArrow)) {
+			this.transform.Translate (0.0f, 0.1f, 0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.DownArrow)) {
+			this.transform.Translate (0.0f, -0.1f, 0.0f);
+		}
+
+		if (Input.GetKey (KeyCode.LeftArrow)) {
+			this.transform.Translate (0.0f, 0.0f, -0.2f);
+		}
+
+		if (Input.GetKey (KeyCode.RightArrow)) {
+			this.transform.Translate (0.0f, 0.0f, 0.2f);
+		}
+
+		//Si el usuario presiona la tecla espacio
+		if (Input.GetKeyDown (KeyCode.Space)) {
+
+			GameObject clone = GameObject.Instantiate (Resources.Load ("Prefabs/Misil") as GameObject);
+		}
+		
+	}
+}
---
+++ CarlosChacon-Actividad4-AirCombat/Assets/Scripts/MissileFalling.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileFalling : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Mover hacia adelante
+		this.transform.Translate(0.0f, 0.0f, 0.5f);
+		
+	}
+	void OnCollisionEnter(Collision collision) {
+		//Eliminamos el piso
+		Destroy (collision.collider.gameObject);
+	}
+}
